b462b1750d7721d5cae6ae92c13c27a1d2673c79
Fernando Medina Corey <fmcorey@gmail.com>
2018-04-10 14:08:38 -0700

    remove some spaces

--- application.py
+++ application.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template
- 
+
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "simple test"
+    try:
+        return render_template("index.html")
+    except Exception as e:
+        return str(e)
 
- 
 if __name__ == "__main__":
     app.run(debug = True)
