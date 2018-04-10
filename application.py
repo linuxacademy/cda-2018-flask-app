903ec64334286b1bd741c0d0c80aae337a58d8bc
Fernando Medina Corey <fmcorey@gmail.com>
2018-04-10 14:33:52 -0700

    Test with appplication instead of app

--- application.py
+++ application.py
@@ -1,8 +1,8 @@
 from flask import Flask, render_template
 
-application = app =  Flask(__name__)
+application =  Flask(__name__)
 
-@app.route('/')
+@application.route('/')
 def home():
     try:
         return render_template("index.html")
